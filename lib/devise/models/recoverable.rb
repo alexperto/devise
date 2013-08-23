b4077b4aee48b8e83da04ff67b330c20e974b932
AleX Cabrera <alexperto@gmail.com>
2013-08-23 13:14:30 -0700

    Deep debugging

--- lib/devise/models/recoverable.rb
+++ lib/devise/models/recoverable.rb
@@ -30,11 +30,14 @@ module Devise
         self.password = new_password
         self.password_confirmation = new_password_confirmation
         if valid?
+          Rails.logger.info "is valid"
           clear_reset_password_token
           after_password_reset
         end
 
-        save( :validate => false )
+        rs = save( :validate => false )
+        Rails.logger.info ">>> save: #{rs}"
+        rs
       end
 
       # Resets reset password token and send reset password instructions by email
